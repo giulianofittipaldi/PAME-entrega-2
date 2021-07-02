9457f387775b4b0abb7eab0a5b40defe419704dd
giulianofittipaldi <giulianofittipaldi@gmail.com>
2021-07-01 21:50:19 -0300

    :wrench: Todas as funcionalidades com ordenações

--- menuTurmas.py
+++ menuTurmas.py
@@ -29,34 +29,55 @@ def menu_Turma():
         elif (opcaoMenuTurma == 3):
             nome_Turma = input("Qual o nome da turma? ")
             nome_Aluno = input("Qual o nome do aluno? ")
-            aluno = funcionalidades.ProcuraLista(ListaAlunos, nome_Aluno)
             turma = funcionalidades.ProcuraLista(ListaTurmas, nome_Turma)
-            turma.AddAluno(aluno)
-            #funcionalidades.AdicionaAluno(ListaTurmas, ListaAlunos, nome_Turma, nome_Aluno)
+            aluno = funcionalidades.ProcuraLista(ListaAlunos, nome_Aluno)
+            if not turma:
+                print("Turma não cadastrada")
+            else:
+                if not aluno:
+                    print("Aluno não cadastrado")
+                else:
+                    turma.AddAluno(aluno)
             
 
         elif (opcaoMenuTurma == 4):
             nome_Turma = input("Qual o nome da turma? ")
             nome_Aluno = input("Qual o nome do aluno? ")
-            aluno = funcionalidades.ProcuraLista(ListaAlunos, nome_Aluno)
             turma = funcionalidades.ProcuraLista(ListaTurmas, nome_Turma)
-            turma.RemvAluno(aluno)
+            aluno = funcionalidades.ProcuraLista(ListaAlunos, nome_Aluno)
+            if not turma:
+                print("Turma não cadastrada")
+            else:
+                if not aluno:
+                    print("Aluno não cadastrado")
+                else:
+                    turma.RemvAluno(aluno)
 
         elif (opcaoMenuTurma == 5):
             nome_Turma = input("Qual o nome da turma? ")
             nome_Aluno = input("Qual o nome do aluno? ")
             nota = int(input("Qual a nota do aluno? "))
-            aluno = funcionalidades.ProcuraLista(ListaAlunos, nome_Aluno)
             turma = funcionalidades.ProcuraLista(ListaTurmas, nome_Turma)
-            aluno.AddNota(nota)
+            aluno = funcionalidades.ProcuraLista(ListaAlunos, nome_Aluno)
+            if not turma:
+                print("Turma não cadastrada")
+            else:
+                if not aluno:
+                    print("Aluno não cadastrado")
+                else:
+                    aluno.AddNota(nota)
 
         elif (opcaoMenuTurma == 6):
             nome_Turma = input("Qual o nome da turma? ")
-            turma = funcionalidades.ProcuraLista(ListaTurmas, nome_Turma)
-            turma.listaAlunosTurma.sort(key=lambda x: x.nome)
-            funcionalidades.printLista(turma.listaAlunosTurma)#a lista de alunos está só com os nomes dos alunos str
+            if not turma:
+                print("Turma não cadastrada")
+            else:
+                turma = funcionalidades.ProcuraLista(ListaTurmas, nome_Turma)
+                turma.listaAlunosTurma.sort(key=lambda x: x.nome)
+                funcionalidades.printLista(turma.listaAlunosTurma)
 
 
         elif (opcaoMenuTurma == 7):
+            ListaTurmas.sort(key=lambda x: len(x.listaAlunosTurma), reverse=True)
             funcionalidades.printLista(ListaTurmas)
     return
